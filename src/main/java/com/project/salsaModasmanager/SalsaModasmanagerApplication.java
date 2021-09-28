7a83137924f9867e79c220ab68ef21706ff84f03
arthur607 <arthurrodriguesg1@gmail.com>
2021-09-27 21:17:19 -0300

    start project

---
+++ src/main/java/com/project/salsaModasmanager/SalsaModasmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.salsaModasmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsaModasmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsaModasmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/salsaModasmanager/SalsaModasmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.salsaModasmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalsaModasmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
