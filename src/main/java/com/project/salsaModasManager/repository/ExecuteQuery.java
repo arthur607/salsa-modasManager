ecbbade87b54668861d190d90a98125983596632
arthur607 <arthurrodriguesg1@gmail.com>
2021-10-02 11:34:51 -0300

    create connection with h2 database

--- src/main/java/com/project/salsaModasManager/repository/ExecuteQuery.java
+++ src/main/java/com/project/salsaModasManager/repository/ExecuteQuery.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class ExecuteQuery<T extends DefaultDAO<T>> {
 
     protected String dataSource;
-JdbcTemplate jdbcTemplate;
+    JdbcTemplate jdbcTemplate;
     public List<T> list(Map<String, String> param, String query, T map) {
         log.info("T => {}", map.getClass().getName());
         log.info("param => {}", param);
