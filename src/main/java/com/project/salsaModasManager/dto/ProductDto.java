aeade8a97a8182e9d363edd2848646d92c8809e1
arthur607 <arthurrodriguesg1@gmail.com>
2021-10-27 11:09:35 -0300

    clean geral e arrumando bug dos DTOs

--- src/main/java/com/project/salsaModasManager/dto/ProductDto.java
+++ src/main/java/com/project/salsaModasManager/dto/ProductDto.java
@@ -7,10 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
 
 @AllArgsConstructor
 @Getter
