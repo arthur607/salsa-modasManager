a8e42a2a99ce79a4616248a1b359f87864184a17
arthur150ml <arthurrodriguesg1@gmail.com>
2021-12-10 22:17:21 -0300

    incrementando frontEnd

--- src/main/java/com/project/salsa_Modas_Manager/controller/Html.java
+++ src/main/java/com/project/salsa_Modas_Manager/controller/Html.java
@@ -1,13 +1,20 @@
 package com.project.salsa_Modas_Manager.controller;
 
+import com.project.salsa_Modas_Manager.model.Produto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.math.BigDecimal;
 
 @Controller
 public class Html {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        Produto produto = new Produto();
+        produto.setPrecoCompra(new BigDecimal(100L));
+
+        model.addAttribute("pr",produto);
         return "/page";
     }
 }
