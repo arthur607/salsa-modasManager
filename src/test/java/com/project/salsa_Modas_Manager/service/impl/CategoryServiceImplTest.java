8a63402a313056ba9c3dcecb1391e1d69b3b64b5
arthur150ml <arthurrodriguesg1@gmail.com>
2021-12-21 09:08:41 -0300

    tratando exceção

--- src/test/java/com/project/salsa_Modas_Manager/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/project/salsa_Modas_Manager/service/impl/CategoryServiceImplTest.java
@@ -1,21 +1,20 @@
 package com.project.salsa_Modas_Manager.service.impl;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
+import com.project.salsa_Modas_Manager.Exception.CategoryNotFoundException;
 import com.project.salsa_Modas_Manager.model.Category;
-import com.project.salsa_Modas_Manager.model.dto.CategoryDto;
 import com.project.salsa_Modas_Manager.repository.jpaRepositories.CategoryRepository;
 import com.project.salsa_Modas_Manager.utils.CategoryConverter;
 import com.project.salsa_Modas_Manager.utils.CategoryConverterImp;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
@@ -31,21 +30,12 @@ class CategoryServiceImplTest {
     @Mock
     CategoryConverter categoryConverter;
 
-    @BeforeEach
-    void setUp() {
-
-    }
-
     @Test
     void testDeleteById() {
         doNothing().when(this.categoryRepository).deleteById((Long) any());
         this.categoryServiceImpl.deleteById(123L);
         verify(this.categoryRepository).deleteById((Long) any());
         assertTrue(this.categoryServiceImpl.findAll().isEmpty());
-    }
-
-    @Test
-    void validCategoryName() {
     }
 
     @Test
@@ -63,10 +53,34 @@ class CategoryServiceImplTest {
 
     @Test
     void findByName() {
+        categoryServiceImpl.findByName(anyString());
+
+        verify(categoryRepository).findByNomeContaining(anyString());
     }
 
     @Test
     void getById() {
+
+        Optional<Category> category = Optional.of(new Category());
+        when(categoryRepository.findById(anyLong())).thenReturn(category);
+        categoryServiceImpl.getById(anyLong());
+
+        verify(categoryRepository).findById(anyLong());
+    }
+
+    @Test
+    void getByIdError() {
+        Optional<Category> category = Optional.of(new Category());
+        when(categoryRepository.findById(anyLong())).thenThrow(CategoryNotFoundException.class);
+
+        try {
+            categoryServiceImpl.getById(anyLong());
+            //verifyNoInteractions(""); cenário de nova implementação
+
+        }catch (CategoryNotFoundException e){
+
+        }
+
     }
 
     @Test
